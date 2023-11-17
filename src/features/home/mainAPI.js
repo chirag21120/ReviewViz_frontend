d7f85b4834c4cf808ab77540d6c829386583fe6b
CHIRAG <cgupta_be21@thapar.edu>
2023-11-17 20:38:48 +0530

    api added

--- src/features/home/mainAPI.js
+++ src/features/home/mainAPI.js
@@ -1,6 +1,13 @@
 // A mock function to mimic making an async request for data
-export function fetchCount(amount = 1) {
-  return new Promise((resolve) =>
-    setTimeout(() => resolve({ data: amount }), 500)
+export function createWordCloud(url) {
+  return new Promise(async (resolve) =>{
+    const response = await fetch('htpp://localhost:8080/word',{
+      method:'GET',
+      body: JSON.stringify({url:url}),
+      headers:{'content-type': 'application/json'}
+    });
+    const data = await response.json();
+    resolve(data);
+  }
   );
 }
