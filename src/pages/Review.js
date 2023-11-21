ebb5913bc943c60742153d72a00fbc40455ecf60
CHIRAG <cgupta_be21@thapar.edu>
2023-11-21 16:52:00 +0530

    review

---
+++ src/pages/Review.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from '../features/navbar/Nav'
+import WordCloud from '../features/home/wordCloud'
+
+function Review() {
+  return (
+    <>
+      <Nav/>
+      <WordCloud></WordCloud>
+    </>
+  )
+}
+
+export default Review
